d80d8466a011e8a0855db939279473af03a1feed
xuanchengwei <1033027640@QQ.com>
2023-07-17 15:06:09 +0800

    测试成功！

--- app/src/main/java/com/xuanchengwei/videotogif/MainActivity.kt
+++ app/src/main/java/com/xuanchengwei/videotogif/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.xuanchengwei.videotogif
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.arthenica.mobileffmpeg.Config
+import com.arthenica.mobileffmpeg.FFmpeg
 import com.xuanchengwei.videotogif.ui.theme.VideoToGifTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +21,17 @@ class MainActivity : ComponentActivity() {
 
         this.getExternalFilesDir("test")
         val testFile = "/storage/emulated/0/Android/data/com.xuanchengwei.videotogif/files/test/360P.mp4"
+        val outputPath = "/storage/emulated/0/Android/data/com.xuanchengwei.videotogif/files/test/out.gif"
+
+        val rc = FFmpeg.execute("-i $testFile $outputPath")
+
+        if (rc == Config.RETURN_CODE_SUCCESS) {
+            Log.i("rc", "命令执行成功")
+        } else if (rc == Config.RETURN_CODE_CANCEL) {
+            Log.i("rc", "用户取消了命令")
+        } else {
+            Log.i("rc", String.format("命令执行失败, 返回值=%d", rc))
+        }
 
         setContent {
             VideoToGifTheme {
