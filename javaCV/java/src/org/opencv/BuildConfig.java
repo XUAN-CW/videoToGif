0dbe743add810b17b9b02f66062c2a83285ac0da
xuanchengwei <1033027640@QQ.com>
2023-07-17 13:44:59 +0800

    修复 BuildConfig, 不知道对不对

---
+++ javaCV/java/src/org/opencv/BuildConfig.java
@@ -0,0 +1,19 @@
+/**
+ * https://stackoverflow.com/questions/76374886/error-cannot-find-symbol-import-org-opencv-buildconfig-android-studio
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.opencv;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "org.opencv";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "org.opencv";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4700;
+  public static final String VERSION_NAME = "4.7.0";
+}
