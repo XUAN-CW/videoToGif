fa178ce1c5ac46fe20bd1d6362a9581778208cd1
xuanchengwei <1033027640@QQ.com>
2023-07-17 14:51:30 +0800

    Revert "测试失败"
    This reverts commit 3ad96145ef3c66d4e3b3135b4562527bf7f44305.

--- app/src/main/java/com/xuanchengwei/videotogif/MainActivity.kt
+++ app/src/main/java/com/xuanchengwei/videotogif/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.xuanchengwei.videotogif
 
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -14,15 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.xuanchengwei.videotogif.ui.theme.VideoToGifTheme
 import org.opencv.android.OpenCVLoader
-import org.opencv.android.Utils
-import org.opencv.core.Mat
-import org.opencv.core.MatOfByte
-import org.opencv.core.Size
-import org.opencv.imgproc.Imgproc
-import org.opencv.videoio.VideoCapture
-import org.opencv.videoio.VideoWriter
-import org.opencv.videoio.Videoio
-import java.io.ByteArrayOutputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,9 +25,6 @@ class MainActivity : ComponentActivity() {
             Log.i("OpenCVLoader","F")
 
         }
-
-        convertMp4ToGif("/storage/emulated/0/Android/data/com.xuanchengwei.videotogif/files/test/360P.mp4", "/storage/emulated/0/Android/data/com.xuanchengwei.videotogif/files/test/360P.gif")
-
         setContent {
             VideoToGifTheme {
                 // A surface container using the 'background' color from the theme
@@ -50,39 +37,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-fun convertMp4ToGif(mp4FilePath: String, gifFilePath: String) {
-    val videoCapture = VideoCapture(mp4FilePath)
-    if (!videoCapture.isOpened) {
-        // Handle error opening the MP4 file
-        return
-    }
-
-    val frameSize = Size(
-        videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH),
-        videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT)
-    )
-
-    val fourcc = VideoWriter.fourcc('G', 'I', 'F', 'S')
-
-    val videoWriter = VideoWriter()
-    videoWriter.open(gifFilePath, fourcc, 10.0, frameSize, true)
-
-    val frame = Mat()
-    val convertedFrame = Mat()
-
-    while (videoCapture.read(frame)) {
-        // Convert the frame to the desired format (e.g., grayscale or resize if needed)
-        // Here, we convert the frame to grayscale
-        Imgproc.cvtColor(frame, convertedFrame, Imgproc.COLOR_BGR2GRAY)
-
-        // Write the converted frame to the GIF
-        videoWriter.write(convertedFrame)
-    }
-
-    videoCapture.release()
-    videoWriter.release()
 }
 
 @Composable
